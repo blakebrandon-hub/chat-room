dd161bf6b1f5965ec8b5583754d708b237a5507b
blakebrandon-hub <50201165+blakebrandon-hub@users.noreply.github.com>
2021-03-14 18:37:26 -0600

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,5 +1,13 @@
 //Connect socket
-const socket = io.connect('http://0.0.0.0:5000');
+//const socket = io.connect('http://0.0.0.0:5000');
+
+var socket = io();
+var el;
+
+socket.on('time', function(timeString) {
+  el = document.getElementById('server-time')
+  el.innerHTML = 'Server time: ' + timeString;
+});
 
 //Get DOM elements
 var message = document.getElementById('message');
